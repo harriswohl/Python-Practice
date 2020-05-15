8ce87e35aefbd08acb7d80bd9893d99cf263ac5f
Harris Wohl <53989269+harriswohl@users.noreply.github.com>
2020-05-15 15:55:40 -0400

    Fixed bug with total bill amount
    changed variable totalamount to billamount, so now totalamount includes the tip.

--- hw_splitbill.py
+++ hw_splitbill.py
@@ -1,13 +1,15 @@
 try:
-    totalamount = float(input("Bill amount in dollars: "))
+    billamount = float(input("Bill amount in dollars: "))
     tip = float(input("Percent tip: "))
     people = float(input("Number of people: "))
 except ValueError:
     quit("Please enter a numerical value.")
 
+
+totalamount = billamount + tip
 print("The tip is {:.2f} dollars".format(tip))
 print("The total cost is {:.2f} dollars.".format(totalamount))
 
-perperson = (totalamount + tip)/ people
+perperson = totalamount/ people
 
 print("The cost per person is {:.2f} dollars".format(perperson))
