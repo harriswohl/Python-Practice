23755533aabfb16241a0d7f99a9d18493b3c4ef8
Harris Wohl <53989269+harriswohl@users.noreply.github.com>
2020-05-23 15:37:16 -0400

    Add Lab 2 diagonal program

---
+++ hw_diagonal.py
@@ -0,0 +1,14 @@
+#takes user input as a string and displays 
+
+#the string diagonally, 3 characters at a time
+userinput = input("text: ")
+
+def main(text):
+    i = 0 
+    numspaces = 0
+    while i < (len(text)-2):
+        line = numspaces * " " + text[i:i + 3]
+        print(line)
+        i += 1
+        numspaces += 3
+main(userinput)
